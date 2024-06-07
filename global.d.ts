3cf8a70f1f851391418a6bb17951abb0158cbd5c
Prashan Fernando <prashjanfdo@gmail.com>
2024-06-07 14:51:49 +0100

    updated

---
+++ global.d.ts
@@ -0,0 +1,3 @@
+interface Window {
+  _ckm: any; // or specify the appropriate type instead of 'any'
+}
--- shopify-cake-newsletter-integration/index.ts
+++ shopify-cake-newsletter-integration/index.ts
@@ -1,1 +1,57 @@
-console.log("Hello Shopify Cake Newsletter Integration", new Date());
+declare var CKMLib: any;
+
+console.log("Hello Shopify Cake Newsletter Integration");
+(() => {
+  function onElementAdded(callback: () => void) {
+    const observer = new MutationObserver((mutations) => {
+      mutations.forEach((mutation) => {
+        mutation.addedNodes.forEach((node) => {
+          callback();
+        });
+      });
+    });
+
+    // Observe the document for changes
+    observer.observe(document.documentElement, { childList: true, subtree: true });
+  }
+
+  onElementAdded(function () {
+    var formEl = document.querySelector(".klaviyo-form");
+    if (formEl && !formEl.getAttribute("data-lolatracking")) {
+      formEl.setAttribute("data-lolatracking", "1");
+      formEl.addEventListener("submit", function (e) {
+        console.log("NL tracked");
+        trackEvent();
+      });
+      var submitBtn = formEl.querySelector("button");
+      if (submitBtn) {
+        submitBtn.addEventListener("click", function (e) {
+          console.log("NL tracked");
+          trackEvent();
+        });
+      }
+    }
+  });
+
+  function trackEvent() {
+    window._ckm = window._ckm || [];
+    window._ckm.push(function cfgev() {
+      CKMLib.configureEvents({
+        domain: "https://savarois.com",
+        offer_id: 21024,
+        event_id: 1242,
+      });
+      CKMLib.fireEvent();
+    });
+
+    if (typeof CKMLib !== "string") {
+      CKMLib = "loading";
+      const pix: HTMLScriptElement = document.createElement("script");
+      pix.type = "text/javascript";
+      pix.async = true;
+      pix.src = "//cakecdn.com/jssdk/lib.js";
+      const s: HTMLScriptElement = document.getElementsByTagName("script")[0];
+      s.parentNode?.insertBefore(pix, s);
+    }
+  }
+})();
