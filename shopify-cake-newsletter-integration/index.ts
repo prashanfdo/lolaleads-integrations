1e7ae7664e6f24b1b58c0ead8b4e6166d3d6e172
Prashan Fernando <prashjanfdo@gmail.com>
2024-06-21 08:12:07 +0100

    updated

--- shopify-cake-newsletter-integration/index.ts
+++ shopify-cake-newsletter-integration/index.ts
@@ -39,6 +39,8 @@ var CKMLib: any;
           .some((msg) => text?.toLowerCase().includes(msg.toLowerCase()))
       ) {
         scriptEl?.setAttribute("data-lolaleads-tracked", "1");
+        console.log("lolaleads-nls - pre");
+        trackView();
         console.log("lolaleads-nls -");
         trackEvent();
         console.log("lolaleads-nls 1");
@@ -57,6 +59,50 @@ var CKMLib: any;
 
     // Observe the document for changes
     observer.observe(document.documentElement, { childList: true, subtree: true });
+  }
+
+  function trackView() {
+    const cookieDomain = "privacyprotectsend.com";
+    var c = {
+      name: "cp",
+      args: {
+        sess_timeout: "",
+        gclid_param: "gclid",
+        gclid_chk_ref: 0,
+        bingkw_param: "ckm_kw",
+        sc_param: "ckmsc",
+        bingkw_chk_ref: 0,
+        attr_param: "ckmscn",
+        attr_chk_ref: 0,
+        sess_param: "",
+        sess_chk_ref: 0,
+        crtv_id: null,
+        crtv_param: "ckmc",
+        crtv_chk_ref: 0,
+        tpcrid_param: "ckmcrn",
+        cookie_dom: cookieDomain,
+        ref_type: 1,
+        ckm_sess_param: null,
+        sl_param: "s1",
+        s2_param: "s2",
+        s3_param: "s3",
+        s4_param: "s4",
+        s5_param: "s5",
+        sub_chk_ref: 0,
+      },
+    };
+
+    var params = new URLSearchParams(window.location.search);
+
+    var thirdPartyID = params.get("ckmscn");
+    var creativeID = params.get("ckmc");
+    var activeTracking = params.get("ckmat");
+    if (thirdPartyID || creativeID || activeTracking)
+      if (typeof CKMLib == "object") CKMLib.run(c);
+      else {
+        window._ckm = window._ckm || [];
+        window._ckm.push(c);
+      }
   }
 
   function trackEvent() {
